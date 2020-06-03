f9217eda56aea3d85562d4314a2d7d16a4ecafb7
Philipp <philipp.lackner@yahoo.de>
2020-06-03 22:32:25 +0200

    Architectural Skeleton

--- app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.androiddevs.runningappyt.ui
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.di.ViewModelProviderFactory
 import com.androiddevs.runningappyt.ui.viewmodels.MainViewModel
 import com.androiddevs.runningappyt.ui.viewmodels.StatisticsViewModel
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -22,7 +26,18 @@ class MainActivity : DaggerAppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
         mainViewModel = ViewModelProvider(this, viewModelProviderFactory).get(MainViewModel::class.java)
         statisticsViewModel = ViewModelProvider(this, viewModelProviderFactory).get(StatisticsViewModel::class.java)
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id) {
+                    R.id.setupFragment, R.id.trackingFragment -> bottomNavigationView
+                        .visibility = View.GONE
+                    else -> bottomNavigationView.visibility = View.VISIBLE
+                }
+            }
     }
 }
--- app/src/main/java/com/androiddevs/runningappyt/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/RunFragment.kt
@@ -1,8 +1,18 @@
 package com.androiddevs.runningappyt.ui.fragments
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.androiddevs.runningappyt.R
-import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_run.*
 
 class RunFragment : BaseFragment(R.layout.fragment_run) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
